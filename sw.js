553c77e699cfc46c0d96df6e8e2103358c4eb322
granjaatuelcodorniz-cloud <granjaatuel.codorniz@gmail.com>
2026-02-20 20:08:14 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,23 +1,12 @@
-const CACHE = 'granja-atuel-v4';
-
+const CACHE = 'granja-atuel-v5';
 self.addEventListener('install', e => {
-  e.waitUntil(
-    caches.open(CACHE).then(c => c.addAll(['./index.html', './manifest.json']))
-  );
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(['./index.html','./manifest.json'])));
   self.skipWaiting();
 });
-
 self.addEventListener('activate', e => {
-  e.waitUntil(
-    caches.keys().then(ks => Promise.all(
-      ks.filter(k => k !== CACHE).map(k => caches.delete(k))
-    ))
-  );
+  e.waitUntil(caches.keys().then(ks => Promise.all(ks.filter(k => k !== CACHE).map(k => caches.delete(k)))));
   self.clients.claim();
 });
-
 self.addEventListener('fetch', e => {
-  e.respondWith(
-    caches.match(e.request).then(cached => cached || fetch(e.request))
-  );
+  e.respondWith(caches.match(e.request).then(c => c || fetch(e.request)));
 });
